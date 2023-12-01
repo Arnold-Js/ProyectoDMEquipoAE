f513481a38b20e8bc177b90f9e167f6df4778aa5
Arnold-Js <r.a.p23cruzortiz@gmail.com>
2023-12-01 13:26:05 -0600

    primera prueba de Compilacion

--- iTaxiAPP/App.tsx
+++ iTaxiAPP/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Text, View } from 'react-native'
 
-export const App = () => {
+const App = () => {
   return (
     <View>
-
+      <Text>BIENVENIDO A iTaxiAPP</Text>
     </View>
   )
 }
+
+export default App;
