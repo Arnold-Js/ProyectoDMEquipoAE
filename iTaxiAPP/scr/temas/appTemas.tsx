9e8c6cde53f049d692e014d4711e84f3feb80cc8
Arnold-Js <r.a.p23cruzortiz@gmail.com>
2023-12-01 15:51:23 -0600

    adicion de carpeta scr y contenido

---
+++ iTaxiAPP/scr/temas/appTemas.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const colores =  {
+    primary:'#FF00AA'
+}
+
+export const styles = StyleSheet.create({
+    marginGlobal:{
+        marginHorizontal:50
+    },
+    title:{
+        fontSize:30,
+        marginBottom:10
+    },
+    colores:{
+        color: '#FF00AA'
+    },
+})
