e8e1efb09e8ed8cc7d41e4a9c60a6a71b0298677
Arnold-Js <r.a.p23cruzortiz@gmail.com>
2023-12-01 15:41:53 -0600

    se hace uso de temas con style

--- iTaxiAPP/App.tsx
+++ iTaxiAPP/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { Pantalla1Principal } from './scr/pantallas/Pantalla1Principal'
+import { colores, styles} from './scr/temas/appTemas'
 
 const App = () => {
   return (
-    <View>
-      <Text>BIENVENIDO A iTaxiAPP</Text>
-    </View>
+    
+      <View>
+        <Text style={styles.title}>BIENVENIDO A iTaxiAPP</Text>
+      </View>
   )
 }
 
