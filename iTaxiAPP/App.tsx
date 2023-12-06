3e0a9c19f07d2e9ceaf9392eb51cf62ffafb97df
Arnold-Js <r.a.p23cruzortiz@gmail.com>
2023-12-05 23:58:28 -0600

    App.tsx

--- iTaxiAPP/App.tsx
+++ iTaxiAPP/App.tsx
@@ -1,14 +1,36 @@
+import 'react-native-gesture-handler';
+
 import React from 'react'
-import { Text, View } from 'react-native'
-import { Pantalla1Principal } from './scr/pantallas/Pantalla1Principal'
-import { colores, styles} from './scr/temas/appTemas'
+import { NavigationContainer } from '@react-navigation/native';
+// import { StackNavigation } from './src/navigation/StackNavigation';
+// import { MenuLateralBasico } from './src/navigation/MenuLateralBasico';
+import { MenuLateral } from './scr/navegacion/menuLateral';
+import { Tabs } from './scr/navegacion/Tabs';
+import { AuthProvider } from './scr/context/AuthContext';
+//import { AuthProvider } from './src/context/AuthContext';
+
+
 
 const App = () => {
   return (
+    <NavigationContainer>
+      <AppState>
+        {/* <Text>Hola navegación</Text> */}
+        {/* <StackNavigation /> */}
+        {/* <MenuLateralBasico /> */}
+        <MenuLateral />
+        {/* <Tabs /> */}
+      </AppState>
+    </NavigationContainer>
     
-      <View>
-        <Text style={styles.title}>BIENVENIDO A iTaxiAPP</Text>
-      </View>
+  )
+}
+
+const AppState = ({children}: any) => {
+  return (
+    <AuthProvider>
+      {children}
+    </AuthProvider>
   )
 }
 
